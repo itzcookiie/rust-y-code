5db186d6a8dc69b4cf86e3550c3e53f7c4e6d5aa
ChildOfGod2 <mayowaadeniyi@hotmail.com>
2021-07-25 19:56:40 +0100

    feat: finish vanilla database project
    add create, delete database operations for models person and company

--- db/src/bin/vanilla-mysql.rs
+++ db/src/bin/vanilla-mysql.rs
@@ -1,1 +1,75 @@
-fn main() {}
+use mysql::*;
+use mysql::prelude::Queryable;
+use db::{Company,Person};
+
+fn main() {
+    let company_a = Company{id : 1};
+    let url = "mysql://mayowa:abc123@localhost:3306/rust";
+    let opts = Opts::from_url(url).unwrap();
+    let pool = Pool::new(opts).unwrap();
+
+    let mut conn = pool.get_conn().unwrap();
+
+    let persons = vec![
+        createPerson(1, String::from("Bob"), 1),
+        createPerson(2, String::from("Judy"), 1),
+        createPerson(3, String::from("Mike"), 1),
+    ];
+
+    dropTables(&mut conn);
+    createTables(&mut conn);
+
+    conn.exec_drop(
+        r"INSERT INTO company VALUES (:id)", params!{
+            "id" => company_a.id
+        }
+    );
+
+    conn.exec_batch(
+        r"INSERT INTO person VALUES (:id, :name, :company_id)",
+        persons.iter().map(|person| params! {
+            "id" => person.id,
+            "name" => &person.name,
+            "company_id" => person.company_id
+        })
+    );
+}
+
+fn createPerson(id : u64, name : String, company_id : u64) -> Person {
+    return Person{
+        id,
+        name,
+        company_id
+    }
+}
+
+fn createTables(conn : &mut PooledConn) {
+    conn.query_drop(
+        r"CREATE TABLE company (
+            id INT NOT NULL,
+            PRIMARY KEY (id)
+        )"
+    );
+
+    conn.query_drop(
+        r"CREATE TABLE person (
+            id int not null,
+            name text not null,
+            company_id int not null,
+            PRIMARY KEY (id),
+            FOREIGN KEY (company_id) REFERENCES company(id)
+        )"
+    );
+}
+
+fn dropTables(conn : &mut PooledConn) {
+    conn.query_drop(
+        r"DROP TABLE companies"
+    );
+    conn.query_drop(
+        r"DROP TABLE persons"
+    );
+    conn.query_drop(
+        r"DROP TABLE company"
+    );
+}
---
+++ db/src/lib.rs
@@ -0,0 +1,9 @@
+pub struct Person {
+    pub id : u64,
+    pub name : String,
+    pub company_id : u64,
+}
+
+pub struct Company {
+    pub id : u64,
+}
